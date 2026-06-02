ecde8f9d3551b5be407747ddb098d6c7cb731746
zei-abhi-test <usetoshiba27@gmail.com>
2026-06-02 19:21:14 +0530

    Port update commit

--- src/main.py
+++ src/main.py
@@ -161,7 +161,7 @@ def serve(path):
 
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 5000))
+    port = int(os.getenv("PORT", 10000))
     debug = os.getenv("FLASK_ENV") == "development"
-    app.run(host="127.0.0.1", port=8000, debug=debug)
+    app.run(host="0.0.0.0", port=port, debug=debug)
 
